af4bc4f3db002aa72d4dbd40205fc452fb4ace78
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-08 05:06:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt(){
 
 function submitIt(){
   $('form').on('submit', function(x) {
-  alert('Your form is going to be submitted now');
+  alert('Your form is going to be submitted now.');
 });
 }
 
