bca28d6fde6bd6fabe51348c56f367a3cd330282
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-08 04:54:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,18 @@ function getIt(){
 });
 }
 
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+
+
+
+
+
+
 $(document).ready(function(){
 
 // call functions here
